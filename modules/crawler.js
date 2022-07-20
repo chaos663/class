23f1fb166e528b077baf3655bfb403526f1b9eb3
chaos663 <ssk8571@gmail.com>
2022-07-20 16:15:00 +0900

    22.07.20 80% 완료 크롤링 코드

--- modules/crawler.js
+++ modules/crawler.js
@@ -20,6 +20,8 @@ const launchConfig = {
 let browser = null
 let page = null
 let pageLength = 0
+const pageSelector = `body > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(5) > td > table:nth-child(5) > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(3)`
+
 
 //실행, init
 const launch = async function () {  //비동기 처리
@@ -32,11 +34,11 @@ const launch = async function () {  //비동기 처리
   // 크롤링 할 페이지를 기입한다. 우리는 휴일지킴이약국을 기입힌다.
 
 }
-
+// url 찾차가기
 const goto = async function (url) {
   return await page.goto(url)
 }
-
+// 팝업 닫기
 const checkPopup = async function () {
   const pages = await browser.pages()
   //최신 문법 !  arr.at(-1)하면 맨 마지막 배열의 값을 가져올 수 있다.
@@ -44,6 +46,7 @@ const checkPopup = async function () {
   //pages의 array 길이 찍어보기
   console.log(pages.length)
 }
+
 // name이라고 class 네임을 만듦, name드간 곳에 다 넣어주면 name 함수에서 받을 수 있음
 const evalCode = async function (name) {
   console.log(`#continents > li.${name} > a`)
@@ -52,16 +55,30 @@ const evalCode = async function (name) {
   }, name)
   // ${}하면 문자를 합칠 수 있다.
 }
-
+// 시군구 클릭
 const evalCity = async function (sigungu) {
-  // 해당 엘ㄹ리먼트를 찾을때까지 기다림
+  // 해당 엘리먼트를 찾을때까지 기다림
   await page.waitForSelector(`#continents > li.songpa_gu > a`)
-
   await page.evaluate(function (sigungu) {
     document.querySelector(`#continents > li.songpa_gu > a`).click()
     // 두번 째 엘리먼트를 사용할 것이다.
   }, sigungu)
 }
+
+const getData = async function () {
+  //페이지의 수 만큼 반복 ! 반복시켜서 클릭시킨다. 그리고 필요한 데이터를 뽑을 것이다.
+  //현재 페이지가 1이니까 i는 1부터 시작해서 1씩 늘어나고 페이지수 만큼 반복
+  for (let i = 1; i < pageLength; i++) {
+    console.log('i:', i)
+    await page.waitForSelector(pageSelector)
+    // getPageLength의 부모와 같은 js path
+    // 인자로 i가 들어가고 i를 return
+    await page.evaluate(function (i,pageSelector) {
+      document.querySelector(pageSelector).children[i].click() //1~7페이지까지 클릭이 일어나면서 페이지가 바뀐다.
+    },i, pageSelector)
+  }
+}
+
 // alert창 닫기
 const alertClose = async function () {
   //page.on은 dialog라는게 뜨게 되면 반환하겠다라는 말
@@ -72,14 +89,16 @@ const alertClose = async function () {
 
 // page 갯수를 먼저 구해보자
 const getPageLength = async function () {
-  await page.waitForSelector(`body > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(5) > td > table:nth-child(5) > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(3)`)
-  
-  pageLength = await page.evaluate(function(){
-    const result = document.querySelector(`body > table:nth-child(2) > tbody > tr > td:nth-child(1) > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(5) > td > table:nth-child(5) > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(3)`).children.length       //7 
-    return result
+  // 해당 샐렉터 기다림
+  await page.waitForSelector(pageSelector) // 페이지를 나타내는 path의 부모
 
-  })
-  console.log('length:',pageLength)
+  pageLength = await page.evaluate(function (pageSelector) {
+    const result = document.querySelector(pageSelector).children.length       //7 
+    return result
+    // result에 7이 담기니까 7이 반환된다.
+  }, pageSelector)
+  console.log('length:', pageLength)
+  // 콘솔에 pageLength를 출력 !
 }
 
 // 여기서 변수에 함수 다 넣어서 export시키고 index.js에서 호출해서 사용 하는 구조 !
@@ -90,6 +109,12 @@ export {
   checkPopup,
   evalCode,
   evalCity,
-  getPageLength
+  getPageLength,
+  getData
 }
 
+
+
+// document.querySelector("#printZone > table:nth-child(2) > tbody")
+// document.querySelector("#printZone > table:nth-child(2) > tbody tr")
+// // 부등호 > 근접한 테이블을 하나 찾겠다. 엘리멘탈 찾기 
--- temp.js
+++ temp.js
@@ -1,3 +1,25 @@
+// crawler.js의 콘솔에 들어가는 스크립트를 짜보자 
+
+document.querySelectorAll("#printZone > table:nth-child(2) > tbody tr")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 동기적 , 비동기적 코드
+
 // function task(title){
 //     setTimeout(function(){
 //         console.log('a')
@@ -7,8 +29,7 @@
 //     },1000)
 // }
 
-// 1000 >> 10초(밀리초 단위라서...)
-// 동기적
+// //1000 >> 10초(밀리초 단위라서...)
 // function coffeMachine (type, callback) {   //callback 함수 : 2 >> callback 지우개라고 하는데 늘어날수록 지연된다.(a끝나고 b,~c,d~ 실행하는 동작)
 //     setTimeout(function(){
 //         console.log(type,':done')
@@ -24,17 +45,17 @@
 //     })
 // })
 
-// 이걸 보완하기 위해서 나온 것이 Promise() 함수
-// 비동기적 함수 만들기
-function asyncTask(arg){
-    const promise = new Promise(function(resolve,reject){  //promise의 매개인자 resolve, reject
-        setTimeout(function(){
-            console.log(arg)
-            resolve()
-        }, 1000)
-    })    //promise에 Promise 함수를 넣자.
-    return promise      //promise를 return시키자 !
-}
+// // 이걸 보완하기 위해서 나온 것이 Promise() 함수
+// // 비동기적 함수 만들기
+// function asyncTask(arg){
+//     const promise = new Promise(function(resolve,reject){  //promise의 매개인자 resolve, reject
+//         setTimeout(function(){
+//             console.log(arg)
+//             resolve()
+//         }, 1000)
+//     })    //promise에 Promise 함수를 넣자.
+//     return promise      //promise를 return시키자 !
+// }
 
 // asyncTask('task a')
 // asyncTask('task b')
@@ -54,15 +75,20 @@ function asyncTask(arg){
 // })
 
 
-// async await 함수(then을 붙인 것과 같이 실행이 된다. 즉,순차적으로 다음 task를 진행시키고 싶을 때 사용하면 된다.)
-await asyncTask('Task A')
-await asyncTask('Task B')
-await asyncTask('Task C')
-await asyncTask('Task D')
+// // async await 함수(then을 붙인 것과 같이 실행이 된다. 즉,순차적으로 다음 task를 진행시키고 싶을 때 사용하면 된다.)
+// await asyncTask('Task A')
+// await asyncTask('Task B')
+// await asyncTask('Task C')
+// await asyncTask('Task D')
 
-// async 와 await를 쓰는 조건이 있다. 
-//(example) async가 promise()함수를 반환시키는 함수
+// // async 와 await를 쓰는 조건이 있다. 
+// // (example) async가 promise()함수를 반환시키는 함수
 // async function test(){
 //     await asyncTask('task 1')  //원래 async의 함수 내에서 await는 실행이 됐어야 했는데
 // }
 //  await asyncTask() // 이거 처럼 그냥 함수 밖에서 await를 쓸 수 있게 되었다.
+
+
+
+
+
